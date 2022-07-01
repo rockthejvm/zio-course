b02ca3f69c2da79fc499e949b89d218c8c57b41d
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2022-07-01 10:11:54 +0300

    fibers exercises

--- src/main/scala/com/rockthejvm/part3concurrency/Fibers.scala
+++ src/main/scala/com/rockthejvm/part3concurrency/Fibers.scala
@@ -1,7 +1,9 @@
 package com.rockthejvm.part3concurrency
 
-import zio._
-import com.rockthejvm.utils._
+import zio.*
+import com.rockthejvm.utils.*
+
+import java.io.{File, FileWriter}
 
 object Fibers extends ZIOAppDefault {
 
@@ -64,5 +66,84 @@ object Fibers extends ZIOAppDefault {
     message <- fiber.join
   } yield message
 
-  def run = chainedFibers.debugThread
+
+  /**
+   * Exercises
+   */
+  // 1 - zip two fibers without using the zip combinator
+  // hint: create a fiber that waits for both
+  def zipFibers[E,A,B](fiber1: Fiber[E,A], fiber2: Fiber[E,B]): ZIO[Any, Nothing, Fiber[E,(A, B)]] = {
+    val finalEffect = for {
+      v1 <- fiber1.join
+      v2 <- fiber2.join
+    } yield (v1, v2)
+    finalEffect.fork
+  }
+
+  val zippedFibers_v2 = for {
+    fib1 <- ZIO.succeed("Result from fiber 1").debugThread.fork
+    fib2 <- ZIO.succeed("Result from fiber 2").debugThread.fork
+    fiber <- zipFibers(fib1, fib2)
+    tuple <- fiber.join
+  } yield tuple
+
+  def zipFibersGeneral[E,E1 <: E,E2 <: E,A,B](fiber1: Fiber[E1,A], fiber2: Fiber[E2,B]): ZIO[Any, Nothing, Fiber[E,(A, B)]] = {
+    // same impl
+    val finalEffect = for {
+      v1 <- fiber1.join
+      v2 <- fiber2.join
+    } yield (v1, v2)
+    finalEffect.fork
+  }
+
+  // 2 - same thing with orElse
+  def chainFibers[E,A](fiber1: Fiber[E,A], fiber2: Fiber[E,A]): ZIO[Any, Nothing, Fiber[E, A]] =
+    fiber1.join.orElse(fiber2.join).fork
+
+  // 3 - distributing a task in between many fibers
+  // spawn n fibers, count the n of words in each file,
+  // then aggregate all the results together in one big number
+
+  def generateRandomFile(path: String): Unit = {
+    val random = scala.util.Random
+    val chars = 'a' to 'z'
+    val nWords = random.nextInt(2000) // at most 2000 random words
+
+    val content = (1 to nWords)
+      .map(_ => (1 to random.nextInt(10)).map(_ => chars(random.nextInt(26))).mkString) // one word for every 1 to nWords
+      .mkString(" ")
+
+    val writer = new FileWriter(new File(path))
+    writer.write(content)
+    writer.flush()
+    writer.close()
+  }
+
+  // part 1 - an effect which reads one file and counts the words there
+  def countWords(path: String): UIO[Int] =
+    ZIO.succeed {
+      val source = scala.io.Source.fromFile(path)
+      val nWords = source.getLines().mkString(" ").split(" ").count(_.nonEmpty)
+      println(s"Counted $nWords in $path")
+      source.close()
+      nWords
+    }
+
+  // part 2 - spin up fibers for all the files
+  def wordCountParallel(n: Int): UIO[Int] = {
+    val effects: Seq[ZIO[Any, Nothing, Int]] = (1 to n)
+      .map(i => s"src/main/resources/testfile_$i.txt") // paths
+      .map(countWords) // list of effects
+      .map(_.fork) // list of effects returning fibers
+      .map((fiberEff: ZIO[Any, Nothing, Fiber[Nothing, Int]]) => fiberEff.flatMap(_.join)) // list of effects returning values (count of words)
+
+    effects.reduce { (zioa, ziob) =>
+      for {
+        a <- zioa
+        b <- ziob
+      } yield a + b
+    }
+  }
+
+  def run = wordCountParallel(10).debugThread
 }
