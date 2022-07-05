15202887f1d74d0eb28a0211f672e0db49104638
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2022-07-05 07:01:26 +0300

    promise exercises

--- src/main/scala/com/rockthejvm/part4coordination/Promises.scala
+++ src/main/scala/com/rockthejvm/part4coordination/Promises.scala
@@ -93,5 +93,75 @@ object Promises extends ZIOAppDefault {
     } yield ()
   }
 
-  def run = downloadFileWithRefPromise()
+  /**
+   * Exercises
+   * 1. Write a simulated "egg boiler" with two ZIOs
+   *  - one increments a counter every 1s
+   *  - one waits for the counter to become 10, after which it will "ring a bell"
+   *
+   * 2. Write a "race pair"
+   *  - use a Promise which can hold an Either[exit for A, exit for B]
+   *  - start a fiber for each ZIO
+   *  - on completion (with any status), each ZIO needs to complete that Promise
+   *    (hint: use a finalizer)
+   *  - waiting on the Promise's value can be interrupted!
+   *  - if the whole race is interrupted, interrupt the running fibers
+   */
+  def eggBoiler(): UIO[Unit] = {
+    def eggReady(signal: Promise[Nothing, Unit]) = for {
+      _ <- ZIO.succeed("Egg boiling on some other fiber, waiting...").debugThread
+      _ <- signal.await
+      _ <- ZIO.succeed("EGG READY!").debugThread
+    } yield ()
+
+    def tickingClock(ticks: Ref[Int], signal: Promise[Nothing, Unit]): UIO[Unit] = for {
+      _ <- ZIO.sleep(1.second)
+      count <- ticks.updateAndGet(_ + 1)
+      _ <- ZIO.succeed(count).debugThread
+      _ <- if (count >= 10) signal.succeed(()) else tickingClock(ticks, signal)
+    } yield ()
+
+    for {
+      ticks <- Ref.make(0)
+      signal <- Promise.make[Nothing, Unit]
+      _ <- eggReady(signal) zipPar tickingClock(ticks, signal)
+    } yield ()
+  }
+
+  def racePair[R,E,A,B](zioa: => ZIO[R,E,A], ziob: ZIO[R,E,B]):
+    ZIO[R,Nothing,Either[(Exit[E,A], Fiber[E,B]), (Fiber[E,A], Exit[E,B])]] =
+    ZIO.uninterruptibleMask { restore =>
+      for {
+        promise <- Promise.make[Nothing, Either[Exit[E,A], Exit[E,B]]]
+        fibA <- zioa.onExit(exita => promise.succeed(Left(exita))).fork
+        fibB <- ziob.onExit(exitb => promise.succeed(Right(exitb))).fork
+
+        result <- restore(promise.await).onInterrupt {
+          for {
+            interruptA <- fibA.interrupt.fork
+            interruptB <- fibB.interrupt.fork
+            _ <- interruptA.join
+            _ <- interruptB.join
+          } yield ()
+        }
+      } yield result match {
+        case Left(exitA) => Left((exitA, fibB))
+        case Right(exitB) => Right((fibA, exitB))
+      }
+    }
+
+  def demoRacePair() = {
+    val zioa = ZIO.sleep(1.second).as(1).onInterrupt(ZIO.succeed("first interrupted").debugThread)
+    val ziob = ZIO.sleep(2.second).as(2).onInterrupt(ZIO.succeed("first interrupted").debugThread)
+
+    val pair = racePair(zioa, ziob)
+
+    pair.flatMap {
+      case Left((exita, fibb)) => fibb.interrupt *> ZIO.succeed("first won").debugThread *> ZIO.succeed(exita).debugThread
+      case Right((fiba, exitb)) => fiba.interrupt *> ZIO.succeed("second won").debugThread *> ZIO.succeed(exitb).debugThread
+    }
+  }
+
+
+  def run = demoRacePair()
 }
